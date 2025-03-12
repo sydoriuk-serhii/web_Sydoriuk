0799b25e49647f52bde18ba32d1c4da64817416c
Serhii <uezapb@gmail.com>
2025-03-12 23:46:30 +0200

    lab_2

---
+++ l_2/js/header.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const closeMenu = document.querySelector('.close-menu');
+
+    burgerMenu.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show-menu');
+    });
+    closeMenu.addEventListener('click', function() {
+        mobileMenu.classList.remove('show-menu');
+    });
+});
+
